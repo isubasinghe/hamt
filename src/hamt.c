57bfe6bd2e05b22e04f071b403455c2a1d54cc21
isubasinghe <isubasinghe@student.unimelb.edu.au>
2023-12-16 19:25:59 +1100

    page sized hamt_node; implemented find_node

--- src/hamt.c
+++ src/hamt.c
@@ -1,46 +1,58 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include <hamt.h>
 #include <immintrin.h>
 
 // ASSUMING MACHINE IS 64 bits
 // IF NOT: SEE END OF FILE
 
-typedef struct {
-  struct hamt_node_t **vals; // 64 * 64 bits -> 4096 bits
-  size_t valids;
+
+typedef struct hamt_node {
+  struct hamt_node **children; // 64 * 62 bits + 64 bits + 64 bits-> 4096 bits 
+  uint64_t key;
+  void *value;
 } __attribute__((aligned(4))) hamt_node_t;
 
 static void nodescopy(hamt_node_t *from, hamt_node_t *to) {
-  to->valids = from->valids;
   for(unsigned int i=0; i < 32; i++) {
-    __m128i v = _mm_loadu_si128((__m128i *)(from->vals + i));
-    _mm_storeu_si128((__m128i *)(to->vals + i), v);
+    __m128i v = _mm_loadu_si128((__m128i *)(from->children + i));
+    _mm_storeu_si128((__m128i *)(to->children + i), v);
   }
 }
 
-hamt_node_t *create_node() {
+hamt_node_t *create_node(void *value) {
   hamt_node_t *node = aligned_alloc(4, sizeof(hamt_node_t));
-  node->valids = 0;
-  node->vals = NULL;
+  node->value = value;
+  node->children = malloc(sizeof(hamt_node_t)*63);
+  memset(node->children, 0, sizeof(hamt_node_t)*63);
+  return node;
 }
 
+hamt_node_t *find_node(hamt_node_t *root, uintptr_t key, char *found) {
+  uint64_t hash = hash64(key);
+  hamt_node_t *h = root;
+  while(1) {
+    hamt_node_t *curr = h->children[hash & 0b0000000000000000000000000000000000000000000000000000000000111111];
+    if(curr->value == NULL) {
+      *found = false;
+      return curr;
+    }
+    if(key == curr->key) {
+      *found = true;
+      return curr;
+    }
+    h = curr;
+  }
+}
 
 hamt_node_t *hamt_insert(hamt_node_t *from, uintptr_t key, void *value) {
-  hamt_node_t *node = create_node();
+  hamt_node_t *node = create_node(value);
   nodescopy(from, node);
   return node;
 }
-
-hamt_node_t *hamt_delete(uintptr_t key) {
-  hamt_node_t *node = create_node();
-  return node;
-}
-
-void free_node(hamt_node_t *) {
-}
-
 
 
 // END OF FILE: FUCK YOU, MACHINE IS 64 BITS
