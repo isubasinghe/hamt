55428de9841cca70adacd7c5718b3cea56de4c1b
isubasinghe <isitha@pipekit.io>
2025-10-26 16:08:28 +0700

    fix: finished find logic

--- src/hamt.c
+++ src/hamt.c
@@ -26,20 +26,41 @@ static void nodescopy(hamt_node_t *from, hamt_node_t *to) {
 hamt_node_t *create_node(void *value) {
   hamt_node_t *node = aligned_alloc(4, sizeof(hamt_node_t));
   node->value = value;
-  node->children = malloc(sizeof(hamt_node_t)*64);
-  memset(node->children, 0, sizeof(hamt_node_t)*64);
+  node->children = malloc(sizeof(hamt_node_t *)*64);
+  memset(node->children, 0, sizeof(hamt_node_t *)*64);
+  node->key = 0;
   return node;
 }
 
 void free_node(hamt_node_t *node) {
+  free(node->children);
+  free(node);
 }
 
-hamt_node_t *find_node(hamt_node_t *root, uintptr_t key, char *found) {
-  uint64_t hash = hash64(key, 0);
+hamt_node_t *find_node(hamt_node_t *root, uintptr_t key, char *found, uint64_t root_level) {
+  uint64_t level = root_level;
+  hamt_node_t *curr = root;
+  while(1) {
+    uint64_t hash = hash64(key, level);
+    uint64_t index = get_index(hash, level);
+    if(curr->children[index] == NULL) {
+      return NULL;
+    }else if(curr->children[index]->value == NULL) {
+      curr = curr->children[index];
+      level++;
+      continue;
+    }
+
+    if(curr->children[index]->key == key) {
+      return curr->children[index];
+    }else {
+      return NULL;
+    }
+  }
   return NULL;
 }
 
-hamt_node_t *hamt_insert(hamt_node_t *from, uintptr_t key, void *value, uint64_t root_level) {
+hamt_node_t *hamt_upsert(hamt_node_t *from, uintptr_t key, void *value, uint64_t root_level) {
   uint64_t level = root_level;
   hamt_node_t *curr = from;
   while(1) {
@@ -52,13 +73,16 @@ hamt_node_t *hamt_insert(hamt_node_t *from, uintptr_t key, void *value, uint64_t
       return node;
     }else if(curr->children[index]->value == NULL) {
       curr = curr->children[index];
+    } else if(curr->children[index]->key == key) {
+      curr->children[index]->value = value;
+      return curr->children[index];
     }else {
       // collision case
       // handle push down
       hamt_node_t *internal_node = create_node(NULL);
 
-      hamt_insert(internal_node, curr->children[index]->key, curr->children[index]->value, root_level+1);
-      hamt_insert(internal_node, key, value, root_level + 1);
+      hamt_upsert(internal_node, curr->children[index]->key, curr->children[index]->value, root_level+1);
+      hamt_upsert(internal_node, key, value, root_level + 1);
 
       free_node(curr->children[index]);
 
